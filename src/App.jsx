d94b39ac87c6cdab5c000d434b6dd3ea88d7a915
5mitty <43888178+5mitty@users.noreply.github.com>
2024-12-01 17:18:34 -0600

    app.jsx with react-router-dom routes

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+
+import './App.css';
+
+function App() { 
+  return (
+    <Router>
+      <Header />
+      <div className='container'>
+        <div className='main'>
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/resume" element={<Resume />} />
+          </Routes>
+        </div>
+      </div>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
